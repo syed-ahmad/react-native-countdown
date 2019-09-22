ca775cac17b433129811c0cdf922770e74eeae3f
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-22 22:05:32 +0100

    First react component

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import EventList from './EventList';
+
+export default function App() {
+  return (
+    <View>
+      <EventList />
+    </View>
+  );
+}
---
+++ EventList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class EventList extends Component {
+  render() {
+    return <Text>Hello world</Text>;
+  }
+}
+
+export default EventList;
