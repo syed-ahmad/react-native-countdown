473d73c02c208c9efb4a8f69510053a6c8bbec30
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-22 22:22:19 +0100

    Use flat list and bind inline data

--- EventList.js
+++ EventList.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 
 class EventList extends Component {
   render() {
-    return <Text>Hello world</Text>;
+    return (
+      <FlatList
+        data={[{ id: 1, name: 'a' }, { id: 2, name: 'b' }]}
+        renderItem={({ item }) => <Text key={item.id}>{item.name}</Text>}
+      />
+    );
   }
 }
 
