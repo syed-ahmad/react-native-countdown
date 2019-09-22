a4bc742328b652c5b0d4ab73e75df71873843f09
Syed Ahmad <syed-ahmad@outlook.com>
2019-09-23 00:03:00 +0100

    Update UI every second

--- EventList.js
+++ EventList.js
@@ -13,10 +13,20 @@ class EventList extends Component {
   state = { events: [] };
 
   componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now()
+        }))
+      });
+    }, 1000);
+
     const events = require('./db.json').events.map(e => ({
       ...e,
       date: new Date(e.date)
     }));
+
     this.setState({ events });
   }
 
